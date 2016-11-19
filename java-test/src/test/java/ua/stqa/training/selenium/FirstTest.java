7c66241382f1be8f439f17771d5a2c7240297af8
mark0v <amark0vv@gmail.com>
2016-11-19 09:45:21 +0200

    Add first test example

---
+++ java-test/src/test/java/ua/stqa/training/selenium/FirstTest.java
@@ -0,0 +1,23 @@
+package ua.stqa.training.selenium;
+
+/**
+ * Created by Alexander on 11/19/2016.
+ */
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class FirstTest extends TestBase {
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+
+
+}
